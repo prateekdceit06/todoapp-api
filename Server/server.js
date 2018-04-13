438083e33416e287968ae7e2f82180219360fb98
Prateek Jain <prateekdceit06@gmail.com>
2018-04-13 17:23:32 +0530

    Add DELETE /todos/:id

--- Server/server.js
+++ Server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req,res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status('404').send('Invalid todo');
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo) {
+           return res.status(404).send('Todo not found');
+        }
+        return res.send({todo});
+    }).catch((e)=>{
+        return res.status(400).send();
+    });
+});
+
 
 app.listen (port, ()=> {
     console.log (`Started on port ${port}`);
